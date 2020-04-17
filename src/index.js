92c6133544500fd221485185925349ce68d29e53
vishalbollam <60523632+vishalbollam@users.noreply.github.com>
2020-04-16 22:10:14 -0700

    creating React functional component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//ES6, Nested Components and React Tools
+
+function Greeting() {
+  return (
+    <section className="Greeting">
+      <Greet/>
+           
+    </section>
+  );
+}
+const Greet = () => {
+  return (
+    <article className="greet">
+      <CoverImage />
+      <Title />
+      <Occupation/>
+      <p>Enthusiastic Software Developer with good experience in computer programing and complete understanding of entire Software Development life cycle. I am good in demonstrating computational thinking and have an ability to break down large complex problems to support my team and excited to be at the development phase of my career as a developer. I am ambitious and an alliteration advocate.</p>
+    
+    </article>
+    
+  );
+};
+const CoverImage = () => (
+  <img
+    width="200"
+    src="b1.jpg"
+    alt="vishal smiling"
+  />
 );
+const Title = () => <h1 style={{font:'500rem',color:'orange'}}>Vishal Bollam </h1>;
+const occupationStyle={
+  letterSpacing:"10px",
+  color:"black" 
+};
+const Occupation = () => <p style={occupationStyle}> Software Developer</p>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDom.render(<Greeting />, document.getElementById("root"));
